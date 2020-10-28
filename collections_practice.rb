a0e18612003cb98123c96a784d4582cfcf9099e7
Stephen <stephenjsweets@gmail.com>
2020-10-27 21:19:25 -0400

    commit tree a1c71973b6099e7f9d52ba8cfdd82fee8f026152

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort do |a, b|
+        b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(array)
+    temp = array[1]
+    array[1] = array[2]
+    array[2] = temp
+    return array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each { |word| word[2] = '$'}
+end
+
+def find_a(array)
+    array.select { |word| word.start_with?('a') }
+end
+
+def sum_array(array)
+    array.reduce(:+)
+end
+
+def add_s(array)
+    array.each_with_index.collect do |value, index| 
+        value += 's' unless index == 1
+        value
+    end
+end
